63f85ca13d44444c169104f29a6d726e5e0bb1b2
Geoffrey Douilly <g3douilly@gmail.com>
2020-05-14 12:14:27 +0200

    Create arch-hero.jpg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,27 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/seo";
+import Header from "../components/header";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = ({ data }) => {
+  return (
+    <div>
+      <SEO title="Home" />
+      <Header siteTitle={data.site.siteMetadata.title} />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  );
+};
 
-export default IndexPage
+export const query = graphql`
+  query HomePageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`;
+
+export default IndexPage;
