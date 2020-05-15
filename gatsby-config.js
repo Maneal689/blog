1815f0b7ccaba3abadf64c93f379008d36e63abc
Geoffrey Douilly <g3douilly@gmail.com>
2020-05-15 16:54:15 +0200

    Fixed manifest dupe

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,20 +52,6 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
-        display: "minimal-ui",
-        icon: "src/images/arch-hero.jpg", // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
         name: "Manealog",
         short_name: "ML",
         start_url: "/",
@@ -75,6 +61,8 @@ module.exports = {
         icon: "src/images/arch-hero.jpg", // This path is relative to the root of the site.
       },
     },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
     {
       resolve: "gatsby-plugin-offline",
       options: {
